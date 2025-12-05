83be2f167a051ee05e72d5ef67dd75f153b3f388
max-debinsky <maksdeb.kontakt@gmail.com>
2025-12-05 01:06:32 +0100

    implemented vector3

--- include/horizon/vector3.h
+++ include/horizon/vector3.h
@@ -1,32 +1,40 @@
 #pragma once
 
+#include <cmath>
+#include <iostream>
+
 class vector3 {
+public:
     double x, y, z;
 
-    vector3() : x(0), y(0), z(0) {};
-    vector3(double _x, double _y, double _z) : x(0), y(0), z(0) {};
+    vector3();
+    vector3(double _x, double _y, double _z);
 
-    double x() const { return x; }
-    double y() const { return y; }
-    double z() const { return z; }
+    double X() const;
+    double Y() const;
+    double Z() const;
 
-    vector3 operator-() const { return vector3(-x, -y, -z); }
+    vector3 operator-() const;
 
-    vector3& operator+=(const vector3& v) {
-        x += v.x;
-        y += v.y;
-        z += v.z;
-        return *this;
-    }
+    vector3& operator+=(const vector3& v);
+    vector3& operator*=(double t);
+    vector3& operator/=(double t);
 
-    vector3& operator*=(double t) {
-        x *= t;
-        y *= t;
-        z *= t;
-        return *this;
-    }
-
-    vector3& operator/=(double t) {
-        return *this *= 1 / t;
-    }
+    double length_squared() const;
+    double length() const;
 };
+
+using point3 = vector3;
+
+std::ostream& operator<<(std::ostream& out, const vector3& v);
+
+vector3 operator+(const vector3& u, const vector3& v);
+vector3 operator-(const vector3& u, const vector3& v);
+vector3 operator*(const vector3& u, const vector3& v);
+vector3 operator*(double t, const vector3& v);
+vector3 operator*(const vector3& v, double t);
+vector3 operator/(const vector3& v, double t);
+
+double dot(const vector3& u, const vector3& v);
+vector3 cross(const vector3& u, const vector3& v);
+vector3 unit_vector(const vector3& v);
---
+++ src/vector3.cpp
@@ -0,0 +1,78 @@
+#include "horizon/vector3.h"
+
+vector3::vector3() : x(0), y(0), z(0) {}
+vector3::vector3(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
+
+double vector3::X() const { return x; }
+double vector3::Y() const { return y; }
+double vector3::Z() const { return z; }
+
+vector3 vector3::operator-() const { return vector3(-x, -y, -z); }
+
+vector3& vector3::operator+=(const vector3& v) {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+}
+
+vector3& vector3::operator*=(double t) {
+    x *= t;
+    y *= t;
+    z *= t;
+    return *this;
+}
+
+vector3& vector3::operator/=(double t) {
+    return *this *= 1 / t;
+}
+
+double vector3::length_squared() const {
+    return x*x + y*y + z*z;
+}
+
+double vector3::length() const {
+    return std::sqrt(length_squared());
+}
+
+std::ostream& operator<<(std::ostream& out, const vector3& v) {
+    return out << v.x << ' ' << v.y << ' ' << v.z;
+}
+
+vector3 operator+(const vector3& u, const vector3& v) {
+    return vector3(u.x + v.x, u.y + v.y, u.z + v.z);
+}
+
+vector3 operator-(const vector3& u, const vector3& v) {
+    return vector3(u.x - v.x, u.y - v.y, u.z - v.z);
+}
+
+vector3 operator*(const vector3& u, const vector3& v) {
+    return vector3(u.x * v.x, u.y * v.y, u.z * v.z);
+}
+
+vector3 operator*(double t, const vector3& v) {
+    return vector3(t*v.x, t*v.y, t*v.z);
+}
+
+vector3 operator*(const vector3& v, double t) {
+    return t * v;
+}
+
+vector3 operator/(const vector3& v, double t) {
+    return (1/t) * v;
+}
+
+double dot(const vector3& u, const vector3& v) {
+    return u.x*v.x + u.y*v.y + u.z*v.z;
+}
+
+vector3 cross(const vector3& u, const vector3& v) {
+    return vector3(u.y*v.z - u.z*v.y,
+                   u.z*v.x - u.x*v.z,
+                   u.x*v.y - u.y*v.x);
+}
+
+vector3 unit_vector(const vector3& v) {
+    return v / v.length();
+}
